c2b0268a592908d4e8e08e31e4d265a4edd80ca7
Alexis CAROFF <alcaroff@e1r6p4.42.fr>
2018-10-13 19:44:37 +0200

    free in creator

--- src/creator.c
+++ src/creator.c
@@ -6,7 +6,7 @@
 /*   By: alcaroff <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 19:25:20 by alcaroff          #+#    #+#             */
-/*   Updated: 2018/10/12 19:25:46 by alcaroff         ###   ########.fr       */
+/*   Updated: 2018/10/13 18:10:17 by alcaroff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ int		ft_create_room(char *line, int type, t_data *data)
 	if (ft_tablen(tab) != 3
 		|| !ft_isdigit(tab[1][0])
 		|| !ft_isdigit(tab[2][0]))
+	{
+		ft_tabfree(tab);
 		return (1);
+	}
 	ft_strcpy(new.name, tab[0]);
 	new.x = ft_atoi(tab[1]);
 	new.y = ft_atoi(tab[2]);
@@ -38,10 +41,16 @@ int		ft_create_pipe(char *line, t_data *data)
 
 	tab = ft_strsplit(line, '-');
 	if (ft_tablen(tab) != 2)
+	{
+		ft_tabfree(tab);
 		return (1);
+	}
 	if ((new.room[0] = ft_find_room_by_name(data->rooms, tab[0])) == NULL ||
 		(new.room[1] = ft_find_room_by_name(data->rooms, tab[1])) == NULL)
+	{
+		ft_tabfree(tab);
 		return (1);
+	}
 	ft_lstnewadd(&new, sizeof(new), &(data->pipes));
 	ft_tabfree(tab);
 	return (0);
